768b7e4ae17ccd87624e887a12ca06a7607c9251
Anthony Xavier <anthony-xavier@live.com>
2021-12-19 17:32:10 +1000

    Finalized Grid Layout

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,14 @@ import Gallery from './elements/Gallery';
 const App = () => {
   return (
     <div className='container'>
-      <Sidebar className="sidebar" />
-      <div>
-        <Header className="header" />
-        <Realtors className="realtors" />
-      </div>
-      <Features className="features" />
-      <Story className="story" />
-      <Homes className="homes" />
-      <Gallery className="gallery" />
-
-      <Footer className="footer" />
+      <Sidebar />
+      <Header />
+      <Realtors />
+      <Features />
+      <Story />
+      <Homes />
+      <Gallery />
+      <Footer />
     </div>
   )
 }
