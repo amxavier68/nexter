bfe6f333b7f5c239de7691456aa6e9a04d19a671
Anthony Xavier <anthony-xavier@live.com>
2021-12-20 11:13:38 +1000

    Started Story Component

--- src/components/elements/Story.js
+++ src/components/elements/Story.js
@@ -5,11 +5,18 @@ const Story = () => {
   return (
     <div className='story'>
       <div className='story__pictures'>
+
+
         Pictures
       </div>
+
       <div className='story__content'>
-        Content
+        <h3 className='heading-3'>Happy Customers</h3>
+        <h2 className='heading-2 heading-2--light'>&ldquo;The best decision of our lives&rdquo;</h2>
+        <p className='story__text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta!</p>
+        <button className='btn'>Find your own home</button>
       </div>
+
     </div>
   )
 }
