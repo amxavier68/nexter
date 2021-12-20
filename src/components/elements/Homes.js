9facd94c555a67a29631494926bf25f6d4f9c449
Anthony Xavier <anthony-xavier@live.com>
2021-12-20 16:23:08 +1000

    Added Homes Section

--- src/components/elements/Homes.js
+++ src/components/elements/Homes.js
@@ -1,10 +1,214 @@
 import React from 'react';
 import '../App.css';
 
+// House Images
+import House1 from '../../../public/img/house-1.jpeg';
+import House2 from '../../../public/img/house-2.jpeg';
+import House3 from '../../../public/img/house-3.jpeg';
+import House4 from '../../../public/img/house-4.jpeg';
+import House5 from '../../../public/img/house-5.jpeg';
+import House6 from '../../../public/img/house-6.jpeg';
+
 const Homes = () => {
   return (
     <section className='homes'>
-      Homes
+      <div className='home'>
+        <img src={House1} className='home__img' alt='house1' />
+        <span className='home__like'>
+          <i class="far fa-star"></i>
+        </span>
+        <h5 className='home__name'>Beautiful Family House</h5>
+        <div className='home__location'>
+          <span>
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          <p>USA</p>
+        </div>
+        <div className='home__rooms'>
+          <span>
+            <i class="fas fa-users"></i>
+          </span>
+          <p>5 rooms</p>
+        </div>
+        <div className='home__area'>
+          <span>
+            <i class="fas fa-expand-arrows-alt"></i>
+          </span>
+          <p>325 sq<sup>m</sup></p>
+        </div>
+        <div className='home__price'>
+          <span>
+            <i class="fas fa-key"></i>
+          </span>
+          <p>$1,200,000</p>
+        </div>
+        <button className='btn home__btn'>Contact Realtor</button>
+      </div>
+
+      <div className='home'>
+        <img src={House2} className='home__img' alt='house2' />
+        <span className='home__like'>
+          <i class="far fa-star"></i>
+        </span>
+        <h5 className='home__name'>Modern Glass Villa</h5>
+        <div className='home__location'>
+          <span>
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          <p>Canada</p>
+        </div>
+        <div className='home__rooms'>
+          <span>
+            <i class="fas fa-users"></i>
+          </span>
+          <p>6 rooms</p>
+        </div>
+        <div className='home__area'>
+          <span>
+            <i class="fas fa-expand-arrows-alt"></i>
+          </span>
+          <p>450 sq<sup>m</sup></p>
+        </div>
+        <div className='home__price'>
+          <span>
+            <i class="fas fa-key"></i>
+          </span>
+          <p>$2,750,000</p>
+        </div>
+        <button className='btn home__btn'>Contact Realtor</button>
+      </div>
+
+      <div className='home'>
+        <img src={House3} className='home__img' alt='house3' />
+        <span className='home__like'>
+          <i class="far fa-star"></i>
+        </span>
+        <h5 className='home__name'>Cozy Country House</h5>
+        <div className='home__location'>
+          <span>
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          <p>UK</p>
+        </div>
+        <div className='home__rooms'>
+          <span>
+            <i class="fas fa-users"></i>
+          </span>
+          <p>4 rooms</p>
+        </div>
+        <div className='home__area'>
+          <span>
+            <i class="fas fa-expand-arrows-alt"></i>
+          </span>
+          <p>250 sq<sup>m</sup></p>
+        </div>
+        <div className='home__price'>
+          <span>
+            <i class="fas fa-key"></i>
+          </span>
+          <p>$850,000</p>
+        </div>
+        <button className='btn home__btn'>Contact Realtor</button>
+      </div>
+
+      <div className='home'>
+        <img src={House4} className='home__img' alt='house4' />
+        <span className='home__like'>
+          <i class="far fa-star"></i>
+        </span>
+        <h5 className='home__name'>Large Rustical Villa</h5>
+        <div className='home__location'>
+          <span>
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          <p>Portugal</p>
+        </div>
+        <div className='home__rooms'>
+          <span>
+            <i class="fas fa-users"></i>
+          </span>
+          <p>6 rooms</p>
+        </div>
+        <div className='home__area'>
+          <span>
+            <i class="fas fa-expand-arrows-alt"></i>
+          </span>
+          <p>480 sq<sup>m</sup></p>
+        </div>
+        <div className='home__price'>
+          <span>
+            <i class="fas fa-key"></i>
+          </span>
+          <p>$1,950,000</p>
+        </div>
+        <button className='btn home__btn'>Contact Realtor</button>
+      </div>
+
+      <div className='home'>
+        <img src={House5} className='home__img' alt='house5' />
+        <span className='home__like'>
+          <i class="far fa-star"></i>
+        </span>
+        <h5 className='home__name'>Majestic Palace Home</h5>
+        <div className='home__location'>
+          <span>
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          <p>GERMANY</p>
+        </div>
+        <div className='home__rooms'>
+          <span>
+            <i class="fas fa-users"></i>
+          </span>
+          <p>18 rooms</p>
+        </div>
+        <div className='home__area'>
+          <span>
+            <i class="fas fa-expand-arrows-alt"></i>
+          </span>
+          <p>4,230 sq<sup>m</sup></p>
+        </div>
+        <div className='home__price'>
+          <span>
+            <i class="fas fa-key"></i>
+          </span>
+          <p>$9,500,000</p>
+        </div>
+        <button className='btn home__btn'>Contact Realtor</button>
+      </div>
+
+      <div className='home'>
+        <img src={House6} className='home__img' alt='house6' />
+        <span className='home__like'>
+          <i class="far fa-star"></i>
+        </span>
+        <h5 className='home__name'>Modern Family Apartment</h5>
+        <div className='home__location'>
+          <span>
+            <i class="fas fa-map-marker-alt"></i>
+          </span>
+          <p>ITALY</p>
+        </div>
+        <div className='home__rooms'>
+          <span>
+            <i class="fas fa-users"></i>
+          </span>
+          <p>3 rooms</p>
+        </div>
+        <div className='home__area'>
+          <span>
+            <i class="fas fa-expand-arrows-alt"></i>
+          </span>
+          <p>180 sq<sup>m</sup></p>
+        </div>
+        <div className='home__price'>
+          <span>
+            <i class="fas fa-key"></i>
+          </span>
+          <p>$600,000</p>
+        </div>
+        <button className='btn home__btn'>Contact Realtor</button>
+      </div>
     </section>
   )
 }
--- src/components/elements/Story.js
+++ src/components/elements/Story.js
@@ -12,14 +12,12 @@ const Story = () => {
         <img src={Story1} className='story__img--1' alt='couple with new home' />
         <img src={Story2} className='story__img--2' alt='new home' />
       </div>
-
       <div className='story__content'>
         <h3 className='heading-3 mb-sm'>Happy Customers</h3>
         <h2 className='heading-2 heading-2--dark mb-md'>&ldquo;The best decision of our lives&rdquo;</h2>
         <p className='story__text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta!</p>
         <button className='btn'>Find your own home</button>
       </div>
-
     </div>
   )
 }
