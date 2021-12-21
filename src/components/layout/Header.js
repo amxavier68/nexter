9486eb19ec8c7847e3e938782a7280d7e9dfb907
Anthony Xavier <anthony-xavier@live.com>
2021-12-21 13:02:26 +1000

    Added Hero Section

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import '../App.css';
 
+// Image Handling
+import Logo from '../../../public/img/logo.png';
+import Logos1 from '../../../public/img/logo-bbc.png';
+import Logos2 from '../../../public/img/logo-forbes.png';
+import Logos3 from '../../../public/img/logo-techcrunch.png';
+import Logos4 from '../../../public/img/logo-bi.png';
+
 const Header = () => {
   return (
     <header className='header'>
-      Header
+
+      <img src={Logo} className='header__logo' alt='Nexter Logo' />
+      <h3 className='heading-3'>Your own home:</h3>
+      <h1 className='heading-1'>The ultimate personal freedom</h1>
+      <button className='btn header__btn'>View our properties</button>
+      <div className='header__seenon-text'>Seen on</div>
+      <div className='header__seenon-logos'>
+        <img src={Logos1} alt='bbc' />
+        <img src={Logos2} alt='forbes' />
+        <img src={Logos3} alt='techcrunch' />
+        <img src={Logos4} alt='business insider' />
+
+      </div>
+
+
     </header>
   )
 }
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 const Sidebar = () => {
+
+  const [ toggle, setToggle ] = useState(true);
+
+  const openSidebar = () => {
+    console.log('open');
+    setToggle(true);
+  }
+
+  const closeSidebar = () => {
+    console.log('close');
+    setToggle(false);
+  }
+
+
   return (
-    <div className='sidebar'>
-      Sidebar
+    <div className="sidebar">
+      { toggle ? 
+          <button 
+            className="nav-btn--open" 
+            onClick={openSidebar}
+          ><i className="fas fa-angle-right"></i></button>
+        :
+          <button 
+            className='nav-btn--close' 
+            onClick={closeSidebar}
+          ><i className="fas fa-times"></i></button>  
+        }
     </div>
   )
 }
